754a011c4a4d9e3fde5a7c1cd9e1ccca5c9c4737
Bartek <bartekwojtal1207@gmail.com>
2018-06-30 12:50:41 +0200

    Initial commit+ add bootstrap 4

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
