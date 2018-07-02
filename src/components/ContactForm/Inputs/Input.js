8b045ac3d023c3a4e2babd13a97070677f9a9384
Bartek <bartekwojtal1207@gmail.com>
2018-07-02 17:58:24 +0200

    validation

--- src/components/ContactForm/Inputs/Input.js
+++ src/components/ContactForm/Inputs/Input.js
@@ -1,20 +1,24 @@
 import React, {Component} from 'react';
 import './Inputs.css';
-import validator from 'react-validation';
+import Aux from '../../../hoc/Auxs';
+
 
 const input = (props) => {
-    console.log(props.required)
-    return (
-        <div className={'form-control contact-inputs-wrap'}>
-            <input type={props.type}
-                   onChange={props.changed}
-                   name={props.name}
-                   className={'contact-inputs'}
-                   placeholder={props.placeholder}
-                   required={props.required}/>
-            {!props.accept ? <span className={'text-danger'}>{props.errorMessage}</span> : ''}
-        </div>
 
+    return (
+        <Aux>
+            <div className={'form-control contact-inputs-wrap'}>
+
+                <input type={props.type}
+                       onChange={props.changed}
+                       name={props.name}
+                       className={'contact-inputs'}
+                       placeholder={props.placeholder}
+                       required={props.required}/>
+                {props.required ? <span className={'text-danger required-sybmol'}>*</span> : '' }
+                {!props.accept ?  <p className={'text-danger'} >{props.errorMessage}</p> :  ''}
+            </div>
+        </Aux>
     )
 };
 
