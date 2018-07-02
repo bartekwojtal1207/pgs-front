fcb5f0d026242f0185ec14ab7a41fb4e33aafad7
Bartek <bartekwojtal1207@gmail.com>
2018-07-02 21:17:57 +0200

    about us section 100% completted

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
   render() {
     return (
         <Layouts>
-            {/*<AboutUs/>*/}
+            <AboutUs/>
             {/*<SkiCams/>*/}
-            <Contact/>
+            {/*<Contact/>*/}
         </Layouts>
     );
   }
