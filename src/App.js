9f70cfd6945cde768255ccd49aaf03b3fc7e85a1
Bartek <bartekwojtal1207@gmail.com>
2018-07-03 20:23:36 +0200

    switching pages without reload app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Layouts from './components/Layouts/Layouts';
 import AboutUs from './containers/AbouUs/AbouUs';
 import SkiCams from './containers/SkiCams/SkiCams';
--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './NavigationItem.css';
+import { Link } from 'react-router-dom';
 
 
 const naviagationItem = (props) => (
     <li className={'navigation-item'}>
-        <a href={props.link} className={props.activeLink ? 'active' : null}>{props.children}</a>
+        <Link to={props.link} className={props.activeLink ? 'active' : null}>{props.children}</Link>
     </li>
 );
 
