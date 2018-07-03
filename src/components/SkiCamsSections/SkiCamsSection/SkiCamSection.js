8bbec831863ac97486bd9fdfeab7d1e568ece368
Bartek <bartekwojtal1207@gmail.com>
2018-07-03 21:31:09 +0200

    style

--- src/components/SkiCamsSections/SkiCamsSection/SkiCamSection.js
+++ src/components/SkiCamsSections/SkiCamsSection/SkiCamSection.js
@@ -6,8 +6,8 @@ const SkiCamsSection = (props) => {
         <div className={'SkiCamsSection'}>
             <span className={'clock text-right'}>{props.clock}</span>
             <h6 className={'SkiCamsTitle text-center'}>{props.children}</h6>
-            <img width="400" height="220" src={props.video} />
-            <img width="400" height="220" src={props.video1 }/>
+            <img width="300" height="160" src={props.video} />
+            <img width="300" height="160" src={props.video1 }/>
         </div>
     )
 };
